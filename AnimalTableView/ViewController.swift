2298d535cd616fe4fdf8318f15e98f02eaf8537d
Sadidur Rahman Shaikat <mdsadidurrahman74@gmail.com>
2021-04-09 21:28:08 +0600

    test

---
+++ AnimalTableView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AnimalTableView
+//
+//  Created by Shaikat on 4/4/21.
+//  Copyright © 2021 Shayla.18. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController , UITableViewDataSource, UITableViewDelegate
+{
+
+    
+    @IBOutlet weak var AnimalTblVw: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+    tableview
+    
+}
+
